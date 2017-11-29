5f50edb442683d22a75acd9a495c9d1a7f51dba2
Jonas Laursen <jona8690@edu.eal.dk>
2017-11-29 13:32:45 +0100

    Free Internet added

---
+++ FreeTheInternet/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        string url = @"http://jlab13.eal.dk:11105/poll?field=No";
+
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+
+        void Run()
+        {
+            List<Thread> threads = new List<Thread>();
+            for (int i=0; i< 3;i++)
+            {
+                Thread x = new Thread(CallWebsite);
+                x.Start();
+                threads.Add(x);
+            }
+            Console.ReadLine();
+        }
+
+        void CallWebsite()
+        {
+            while (true)
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            }
+        }
+    }
+}
